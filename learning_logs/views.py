16f806102f5fa2d2ade9a7d62b42460b874eef2f
Apologise <603061018@qq.com>
2017-07-03 21:27:07 +0800

    添加文章

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -26,7 +26,7 @@ def topic(request, topic_id):
 
 def new_topic(request):
 	"""添加新主题"""
-	if request.mothod != 'POST':
+	if request.method != 'POST':
 		#未提交数据，创建一个表单
 		form = TopicForm()
 	else:
@@ -37,3 +37,52 @@ def new_topic(request):
 			return HttpResponseRedirect(reverse('learning_logs:topics'))
 	context = {'form':form}
 	return render(request, 'learning_logs/new_topic.html', context)
+
+def new_topic(request):
+	"""添加新的主题"""
+	if request.method != 'POST':
+	# 未提交数据，创建一个新表单
+		form = TopicForm()
+	else:
+		#POST提交的数据，对数据进行处理
+		form = TopicForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('learning_logs:topics'))	
+	context = {'form':form}
+	return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+	"""在特定的主题中添加新的条目"""
+	topic = Topic.objects.get(id=topic_id)
+
+	if request.method != 'POST':
+		#未提交数据，创建一个空表单
+		form = EntryForm()
+	else:
+		#POST提交的数据，对数据进行处理
+		form = EntryForm(data=request.POST)
+		if form.is_valid():
+			new_entry = form.save(commit=False)
+			new_entry.topic = topic
+			new_entry.save()
+			return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+	context = {'topic': topic, 'form': form}
+	return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+	"""编辑既有条目"""
+	entry = Entry.objects.get(id=entry_id)
+	topic = entry.topic
+
+	if request.method != 'POST':
+		#初次请求，使用当前条目填充表单
+		form = EntryForm(instance=entry)
+	else:
+		#POST提交的数据，对数据进行处理
+		form = EntryForm(instance=entry, data=request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
+	context = {'entry': entry, 'topic':topic, 'form':form}
+	return render(request, 'learning_logs/edit_entry.html', context)
